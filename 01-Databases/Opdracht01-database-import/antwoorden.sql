a6a8c7d73f0129038da98406eb4533c5fbc0c11a
Lucien Osei <lucien.osei@student.rocva.nl>
2022-02-10 10:31:23 +0100

    MYSQL SELECT

--- 01-Databases/Opdracht01-database-import/antwoorden.sql
+++ 01-Databases/Opdracht01-database-import/antwoorden.sql
@@ -1,1 +1,10 @@
 -- Taak01 - haal de tabel movies op:
+SELECT * FROM `movies`;
+
+SELECT * FROM `jaar2015`;
+SELECT country, score FROM 'jaar2016';
+SELECT region From 'jaar2015';
+SELECT region, score FROM 'jaar2015';
+SELECT score, rank FROM 'jaar2015';
+
+
